2e391a3284f37150b965b3fa78f85d87b427c3ff
slashtechno <77907286+slashtechno@users.noreply.github.com>
2021-10-16 16:31:07 -0500

    Tried to prevent byte code being written

--- hello-twitter.py
+++ hello-twitter.py
@@ -1,6 +1,6 @@
 import tweepy
 import config
-
+PYTHONDONTWRITEBYTECODE = 0
 print("Hello, world!")
 
 CONSUMER_KEY = str(config.CONSUMER_KEY)
@@ -10,4 +10,4 @@ ACCESS_SECRET = str(config.ACCESS_SECRET)
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 twitter = tweepy.API(auth)
-twitter.update_status("Hello, world!")
+# twitter.update_status("Hello, world!")
