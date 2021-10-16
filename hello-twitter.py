3e3396636de388a275beabd4df9f3f2e4c2e7955
slashtechno <77907286+slashtechno@users.noreply.github.com>
2021-10-15 21:56:40 -0500

    Changed how API keys are stored

--- hello-twitter.py
+++ hello-twitter.py
@@ -1,11 +1,12 @@
 import tweepy
+import config
 
 print("Hello, world!")
 
-CONSUMER_KEY = ""
-CONSUMER_SECRET = ""
-ACCESS_KEY = ""
-ACCESS_SECRET = ""
+CONSUMER_KEY = str(config.CONSUMER_KEY)
+CONSUMER_SECRET = str(config.CONSUMER_SECRET)
+ACCESS_KEY = str(config.ACCESS_KEY)
+ACCESS_SECRET = str(config.ACCESS_SECRET)
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 twitter = tweepy.API(auth)
