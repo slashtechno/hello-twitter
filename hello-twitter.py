fef41da5b6a06ec82cd96cae61cb66050c8ebf8f
slashtechno <77907286+slashtechno@users.noreply.github.com>
2021-11-06 06:03:25 -0500

    If id.txt is empty, scan last 20 mentions and store id

--- hello-twitter.py
+++ hello-twitter.py
@@ -34,6 +34,25 @@ api = tweepy.API(auth)
 
 # go through mentions and scan them for a tag ignoring mentions already scanned
 last_scanned_id = retrieve_id(id_file)
+mentions = api.mentions_timeline(tweet_mode="extended")
+if not last_scanned_id:  # Check if there is a stored # ID
+    for mention in reversed(mentions):
+        print(mention.full_text)  # print the mention's text
+        print("ID for the mention above: "+str(mention.id))
+        # Reply to tweets with specified tag/text
+        target_content = "False"
+        while target_content == "False":
+            if "#HelloWorld" in mention.full_text:
+                last_scanned_id = mention.id
+                write_last_id(last_scanned_id, id_file)
+                api.update_status("@"+mention.user.screen_name
+                                  + " Hi", in_reply_to_status_id=mention.id)
+                print("Replied to @" + mention.user.screen_name)
+                target_content = "True"
+            else:
+                target_content = "False"
+
+print("last scanned ID was " + str(last_scanned_id))
 # Only scan mentions after last scanned mention
 mentions = api.mentions_timeline(
     since_id=last_scanned_id, tweet_mode="extended")
